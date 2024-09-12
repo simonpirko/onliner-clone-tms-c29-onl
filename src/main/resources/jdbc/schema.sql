38b4acbcf03b2bb6b675b90f6fe0835ec23043e9
Yakubchyk <ivan.yakubchyk@gmail.com>
2024-09-12 13:15:00 +0200

    2.2 Add autoinkriment

--- src/main/resources/jdbc/schema.sql
+++ src/main/resources/jdbc/schema.sql
@@ -1,59 +1,63 @@
-DROP SCHEMA public CASCADE;
-
-CREATE SCHEMA public;
-
-CREATE TABLE if not exists account
+CREATE TABLE account
 (
-    id       BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id       BIGINT PRIMARY KEY,
     name     VARCHAR(255),
     username VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255)        NOT NULL,
-    type     VARCHAR(50)         NOT NULL,
-    role     VARCHAR(50)         NOT NULL
+    password VARCHAR(255) NOT NULL,
+    type     VARCHAR(50)  NOT NULL,
+    role     VARCHAR(50)  NOT NULL
 );
 
-CREATE TABLE if not exists product
+CREATE TABLE product
 (
-    main_product_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    product_name            VARCHAR(255),
-    product_description     TEXT,
-    product_category        VARCHAR(100),
-    product_image           VARCHAR(255)
+    id          BIGINT PRIMARY KEY,
+    name        VARCHAR(255),
+    description TEXT,
+    category    VARCHAR(100),
+    image       VARCHAR(255)
 );
 
-CREATE TABLE if not exists orderitem
+CREATE TABLE cart
 (
-    main_orderitem_id         BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id         BIGINT PRIMARY KEY,
+    account_id BIGINT,
+    FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE
+);
+
+CREATE TABLE cart_item
+(
+    id         BIGINT PRIMARY KEY,
+    product_id BIGINT,
+    account_id BIGINT,
+    quantity   INTEGER,
+    price      DECIMAL(10, 2),
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE,
+    FOREIGN KEY (account_id) REFERENCES account (id) ON DELETE CASCADE
+);
+
+CREATE TABLE orderItem
+(
+    id         BIGINT PRIMARY KEY,
     product_id BIGINT,
     quantity   INTEGER,
     price      DECIMAL(10, 2),
-    FOREIGN KEY (product_id) REFERENCES product (main_product_id) ON DELETE CASCADE
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
 );
 
-CREATE TABLE if not exists "order"
+CREATE TABLE "order"
 (
-    main_order_id           BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    id           BIGINT PRIMARY KEY,
     orderItem_id BIGINT,
     totalPrice   DECIMAL(10, 2),
     status       VARCHAR(50),
-    FOREIGN KEY (orderItem_id) REFERENCES orderItem (main_orderitem_id) ON DELETE CASCADE
+    FOREIGN KEY (orderItem_id) REFERENCES orderItem (id) ON DELETE CASCADE
 );
 
-CREATE TABLE if not exists shop
+CREATE TABLE shop
 (
-    main_shop_id          BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    shop_title       VARCHAR(255),
-    shop_description TEXT,
+    id          BIGINT PRIMARY KEY,
+    title       VARCHAR(255),
+    description TEXT,
     creator_id  BIGINT,
     FOREIGN KEY (creator_id) REFERENCES account (id) ON DELETE CASCADE
 );
-
-CREATE TABLE IF NOT EXISTS sellerproduct
-(
-    seller_product_id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    product_id BIGINT,
-    shop_id    BIGINT,
-    price      DECIMAL(10, 2),
-    FOREIGN KEY (product_id) REFERENCES product (main_product_id) ON DELETE CASCADE,
-    FOREIGN KEY (shop_id) REFERENCES shop (main_shop_id) ON DELETE CASCADE
-)
