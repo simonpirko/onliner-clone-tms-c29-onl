715e98a68cb3e34adbbafc2a7839c36fff3ca343
NikitaDmitruk <neomouz@icloud.com>
2024-09-13 11:42:35 +0300

    Added: ProductMapper.java

---
+++ src/main/java/by/tms/onlinerclonec29onl/dao/mapper/ProductMapper.java
@@ -0,0 +1,20 @@
+package by.tms.onlinerclonec29onl.dao.mapper;
+
+import by.tms.onlinerclonec29onl.model.Product;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductMapper implements RowMapper<Product> {
+    @Override
+    public Product mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Product product = new Product();
+        product.setId(rs.getInt("id"));
+        product.setName(rs.getString("name"));
+        product.setDescription(rs.getString("description"));
+        product.setCategory(rs.getString("category"));
+        product.setImage(rs.getBytes("image"));
+        return null;
+    }
+}
