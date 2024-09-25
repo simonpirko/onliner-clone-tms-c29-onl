fd16098d1db1ba37465f5746a9008516f0a582b3
MorozovAleksandr <iamaleks.morozov@gmail.com>
2024-09-25 03:22:00 +0300

    refactoring

--- src/main/java/by/tms/onlinerclonec29onl/controller/CatalogController.java
+++ src/main/java/by/tms/onlinerclonec29onl/controller/CatalogController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/*TODO: кэширование категорий?*/
+
 @Controller
 @RequestMapping("/catalog")
 public class CatalogController {
